da742b32c41376bfd6eaefe8cc01b12597c23bc8
Vlastimil Fürst <vlasec@gmail.com>
2019-11-26 13:40:25 +0100

    Reaction and Sweep refactored and documented.

--- src/main/java/cz/vlasec/sudoku/core/solver/Reaction.java
+++ src/main/java/cz/vlasec/sudoku/core/solver/Reaction.java
@@ -1,8 +1,33 @@
 package cz.vlasec.sudoku.core.solver;
 
 import cz.vlasec.sudoku.core.board.Board;
+import cz.vlasec.sudoku.core.board.Rules.Value;
 import cz.vlasec.sudoku.core.board.Tile;
 
+/**
+ * A reaction to something that changed on the board.
+ * The implementation is empty by default, only implement what is needed.
+ */
 public interface Reaction {
-    void reactTo(Board b, Tile tile, SolverCallback callback);
+
+    /**
+     * Reacts to a value being set to a tile.
+     *
+     * @param callback Reactions being made by this solver component must be reported this way.
+     * @param board    The affected board.
+     * @param tile     The affected tile.
+     */
+    default void onValueSet(SolverCallback callback, Board board, Tile tile) {
+    }
+
+    /**
+     * Reacts to a candidate being ruled out on the tile.
+     *
+     * @param callback Reactions being made by this solver component must be reported this way.
+     * @param board    The affected board.
+     * @param tile     The affected tile.
+     * @param value    The value that was ruled out.
+     */
+    default void onCandidateRuledOut(SolverCallback callback, Board board, Tile tile, Value value) {
+    }
 }
--- src/main/java/cz/vlasec/sudoku/core/solver/Sweep.java
+++ src/main/java/cz/vlasec/sudoku/core/solver/Sweep.java
@@ -2,6 +2,14 @@ package cz.vlasec.sudoku.core.solver;
 
 import cz.vlasec.sudoku.core.board.Board;
 
+/**
+ * A sweep through the whole board. Used when reactions are out of clues.
+ */
 public interface Sweep {
-    void sweep(Board b, SolverCallback callback);
+    /**
+     * Sweeps the whole board and tries to find something.
+     * @param callback Changes being made by this solver component must be reported this way.
+     * @param board    The board to sweep.
+     */
+    void sweep(SolverCallback callback, Board board);
 }
